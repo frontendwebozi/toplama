4da2c463058d4fbf8ffd7631f43007e270d6b02b
frontendwebozi <frontendwebozil@gmail.com>
2025-02-08 22:27:24 +0400

    frond and toplama

---
+++ app.js
@@ -0,0 +1,35 @@
+let birinci_eded = 14;
+let ikinci_eded = 4;
+let toplam_sonrasi = 0;
+let yazi_netice_ucun = "neticemiz:"
+
+function topla() {
+    toplam_sonrasi=birinci_eded+ikinci_eded;
+    document.getElementById("netice"). innerHTML = yazi_netice_ucun + toplam_sonrasi
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
